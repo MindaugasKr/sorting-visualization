415df094d6b6dfa44c70063dd42e7713cb556873
MindaugasKr <>
2019-04-10 14:50:15 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,127 @@
+"use strict";
+
+// mostly based on https://gist.github.com/jeromecoupe/0b807b0c1050647eb340360902c3203a
+
+
+// make fast and slow versions - for watch and for build
+
+
+
+
+const gulp = require("gulp");
+// css
+const sass = require("gulp-sass");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+// js
+const eslint = require("gulp-eslint");
+const uglifyjs = require('gulp-uglify');
+const babel = require('gulp-babel');
+// html
+const uglifyhtml = require('html-minifier').minify;
+// live server
+const browsersync = require("browser-sync").create();
+// mario
+const plumber = require("gulp-plumber");
+// other
+const cp = require("child_process");
+// installed but unused:
+// const webpack = require("webpack");
+// const webpackconfig = require("./webpack.config.js");
+// const webpackstream = require("webpack-stream");
+
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init(
+    {
+      server: {
+        baseDir: "./dist/"
+      },
+      port: 3000
+    }
+  );
+  done();
+};
+
+// browserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// CSS tasks
+function css() {
+  return gulp
+    .src("./src/scss/main.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" })) // <<<< padaryt priklausomai nuo build ar watch
+    //.pipe(gulp.dest("./dist/css/main.css"))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./dist/css/"))
+    .pipe(browsersync.stream());
+}
+
+// Lint scripts
+function scriptsLint() {
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format());
+    //.pipe(eslint.failAfterError());
+}
+
+// minify scripts
+function scripts() {
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(plumber())
+/*     .pipe(babel({
+            presets: ['@babel/env']
+        }))
+    .pipe(uglifyjs()) */
+    .pipe(gulp.dest("./dist/js/"))
+    .pipe(browsersync.stream());
+}
+
+// HTML
+function html() {
+  return gulp
+    .src("./src/*.html")
+    .pipe(plumber())
+    //.pipe(uglifyhtml())
+    .pipe(gulp.dest("./dist/"))
+    .pipe(browsersync.stream());
+}
+
+// watch files
+function watchFiles() {
+  gulp.watch("./src/scss/main.scss", css);
+  gulp.watch("./src/js/*.js", gulp.series(scriptsLint, scripts));
+  gulp.watch("./src/*.html", html); // <<< HTML
+  gulp.watch(
+    [
+      "./_includes/**/*",
+      "./_layouts/**/*",
+      "./_pages/**/*",
+      "./_posts/**/*",
+      "./_projects/**/*"
+    ],
+    gulp.series(browserSyncReload)
+);
+}
+
+// complex tasks
+const js = gulp.series(scriptsLint, scripts);
+const build = gulp.parallel(css, js); // <<< HTML
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.css = css;
+exports.js = js;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
---
+++ src/js/main.js
@@ -0,0 +1,242 @@
+"use-strict";
+
+var log = console.log;
+
+class SortingAlgorithms {
+  constructor() {
+    this.algorithms = {
+      "Bubble": {bubble: function*(arr) {
+        let temp;
+        for (let x = 0; x < arr.length - 1 ; x++) {
+          for (let y = 0; y < arr.length - x - 1; y++) {
+            if (arr[y] > arr[y + 1]) {
+              temp = arr[y];
+              arr[y] = arr[y + 1];
+              arr[y + 1] = temp;
+            }
+            yield arr;
+          }
+        }
+      }},
+      "Simple": {insertions() {}, selection() {}},
+      "Eficient": {merge() {}, heap() {}, quick() {}}
+    }
+  }
+}
+
+class SortingVisualizer extends SortingAlgorithms {
+  constructor(elemNameState = ".state-name-js",
+              elemNameCycle = ".cycle-no-js",
+              elemNameVisualizer = ".visualizer",
+
+              elemNameAlgorythmList = ".options__algorythm-js",
+              elemNameArrayLen = ".options__array-length-js",
+              elemNameMaxVal = ".options__max-value-js",
+              elemNameSpeed = ".options__speed-js",
+              
+              elemNameBtnSartStop = ".btn-start-stop-js",
+              elemNameBtnApplyAndReset = ".btn-apply-changes-js") {
+    super();
+
+    // DOM elements
+    this.elemState = document.querySelector(elemNameState);
+    this.elemCycle = document.querySelector(elemNameCycle);
+
+    this.elemVisualizer = document.querySelector(elemNameVisualizer);
+
+    this.elemAlgorythmList = document.querySelector(elemNameAlgorythmList);
+    this.elemArrayLen = document.querySelector(elemNameArrayLen);
+    this.elemMaxVal = document.querySelector(elemNameMaxVal);
+    this.elemSpeed = document.querySelector(elemNameSpeed);
+
+    this.elemBtnSartStop = document.querySelector(elemNameBtnSartStop);
+    this.elemBtnApplyAndReset = document.querySelector(elemNameBtnApplyAndReset);
+
+
+    // App variables
+    this.array = undefined;
+    this.appState = undefined; // possible states: "running", "finished", "paused", "reset". Used to make in function decisions and to inform user of what is happening;
+    this.arrGenerator = undefined;
+    this.appInterval = undefined;
+
+    // Options
+    this.activeAlgorithm = undefined;
+    this.arrLength = undefined;
+    this.arrMaxValue = undefined;
+    this.speed = undefined;
+
+    // Constants
+    this.ARR_LENGTH_MIM = 2;
+    this.ARR_LENGTH_MAX = 500; 
+    this.SPEED_MIN = 1;
+    this.SPEED_MAX = 100;
+    this.ARR_MAX_VALUE_MIN = 2;
+    this.ARR_MAX_VALUE_MAX = this.elemVisualizer.width; // with max elements bar width is 1px;
+
+    // Canvas variables
+    this.canvasCtx = this.elemVisualizer.getContext('2d', { alpha: false });
+    this.canvasLineWidth = undefined;
+    this.canvasArrMaxValue = undefined;
+ 
+
+    this.__init__();
+  }
+
+  // for initialization
+  populateAlgorithmList() {
+    /** 
+     * Loops over sorting algorithms.
+     * Builds select drop down list.
+     */
+    for (let algGroup in this.algorithms) {
+
+      let optGroup = document.createElement("optgroup");
+      optGroup.label = algGroup;
+
+      for (let alg in this.algorithms[algGroup]) {
+
+        let option = document.createElement("option");
+        option.innerText = this.algorithms[algGroup][alg].name;
+        option.value = `${algGroup},${alg}`;
+
+        optGroup.appendChild(option);
+      }
+
+      this.elemAlgorythmList.appendChild(optGroup);
+    }
+  }
+
+
+  // Check if values are within bounds
+  // Used through event listener
+  enforceValue(e, min, max) {
+    let value = parseInt(e.target.value);
+    if (!Number.isInteger(value)) {
+      e.target.value = min;
+    }
+    else if (value < min) {
+      e.target.value = min;
+    }
+    else if (value > max) {
+      e.target.value = max;
+    }
+  }
+
+  // used by other methods
+  getOptions() {
+    this.activeAlgorithm = this.elemAlgorythmList.value.split(",");
+    this.arrLength = this.elemArrayLen.value;
+    this.arrMaxValue = this.elemMaxVal.value;
+    this.speed = this.elemSpeed.value;
+  }
+  generateArray() {
+    this.array = new Array(parseInt(this.arrLength))
+      .fill(this.arrMaxValue)
+      .map( num => Math.ceil(Math.random()*num) );
+  }
+  // !!!
+  // !!!
+  // !!!
+  // !!!
+  // !!!
+  // !!!
+  // !!!
+  // !!!
+  // kazkur klaida, paz su 56 arr elementais
+  drawCanvas() {
+    let canvasLineWidth, canvasLineWidthHalf, lineYEnd;
+
+    // Calculates line width
+    canvasLineWidth = Math.floor( this.elemVisualizer.width / this.array.length );
+    if (canvasLineWidth < 1) { canvasLineWidth = 1;}
+    // Calculate half of canvasLineWidth, for loop calculations
+    canvasLineWidthHalf = Math.floor(canvasLineWidth / 2);
+
+    // Gets max value of array, for line height calculations
+    this.canvasArrMaxValue = Math.max(...this.array);
+
+    // Canvas setup
+    this.canvasCtx.fillStyle = "white";
+    this.canvasCtx.fillRect(0, 0, this.elemVisualizer.width, this.elemVisualizer.height);
+    //
+    this.canvasCtx.fillStyle = "black";
+    this.canvasCtx.lineWidth = canvasLineWidth;
+
+    // Draw lines
+    for (let n in this.array) {
+      this.canvasCtx.beginPath();
+      this.canvasCtx.moveTo(n*canvasLineWidth + canvasLineWidthHalf, this.elemVisualizer.height);
+      lineYEnd = Math.ceil( this.elemVisualizer.height - (this.elemVisualizer.height * this.array[n] / this.canvasArrMaxValue) );
+      this.canvasCtx.lineTo(n*canvasLineWidth + canvasLineWidthHalf, lineYEnd);
+      this.canvasCtx.stroke();
+    }
+  }
+  updateCanvas() {}
+  updateStateInHTML() {this.elemState.innerText = this.appState;}
+
+  visualizerLoop() {
+    this.array = this.arrGenerator.next().value;
+    if (this.array) {
+      this.elemCycle.innerText = parseInt(this.elemCycle.innerText) + 1;
+      this.drawCanvas();
+    } else {
+      this.appState = "finished";
+      this.updateStateInHTML();
+      clearInterval(this.appInterval);
+    }
+  }
+
+  startApp() {
+    if (this.appState == "finished") {
+      this.ApplyAndReset();
+    }
+    // will run on states finished, paused, reset
+    this.appState = "running";
+    this.updateStateInHTML();
+    this.appInterval = setInterval(this.visualizerLoop.bind(this),1000/this.speed);
+  }
+  stopApp() {
+    this.appState = "paused";
+    this.updateStateInHTML();
+    clearInterval(this.appInterval);
+  }
+
+  // in app button functionality
+  StartStop() {
+    if (this.appState == "running") {
+      this.stopApp();
+    // on states paused, reset, finished
+    } else {
+      this.startApp();
+    }
+   
+  }
+  ApplyAndReset() {
+    if (this.appState == "running") {
+      this.stopApp()}
+    this.elemCycle.innerText = 0;
+    this.appState = "reset";
+    this.updateStateInHTML();
+    this.getOptions();
+    this.generateArray();
+    this.arrGenerator = this.algorithms[this.activeAlgorithm[0]][this.activeAlgorithm[1]](this.array);
+    this.drawCanvas();
+  }
+
+  __init__() {
+    this.populateAlgorithmList();
+
+    // Add event listeners for input elements
+    this.elemArrayLen.addEventListener("blur", (e) => {this.enforceValue(e, this.ARR_LENGTH_MIM, this.ARR_LENGTH_MAX)});
+    this.elemMaxVal.addEventListener("blur", (e) => {this.enforceValue(e, this.ARR_MAX_VALUE_MIN, this.ARR_MAX_VALUE_MAX)});
+    this.elemSpeed.addEventListener("blur", (e) => {this.enforceValue(e, this.SPEED_MIN, this.SPEED_MAX)});
+
+    // Add event listeners for button elements
+    this.elemBtnApplyAndReset.addEventListener("click", this.ApplyAndReset.bind(this));
+    this.elemBtnSartStop.addEventListener("click", this.StartStop.bind(this));
+
+    this.ApplyAndReset();
+  }
+}
+
+var test = new SortingVisualizer();
